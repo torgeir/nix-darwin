e08cb4f79d1925215ede00c4cfc67e1009cd52a2
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-01-01 20:57:23 +0100

    note on colima start --edit

--- home/docker.nix
+++ home/docker.nix
@@ -5,7 +5,7 @@
     # nix 23.11 is colima 0.5.6
     # https://github.com/abiosoft/colima/issues/913
     # try unstable which is colima 0.6.7
-    pkgs.unstable.colima
+    pkgs.unstable.colima # colima start --edit to tune its resources
 
     docker
   ];
