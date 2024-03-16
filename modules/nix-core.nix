e8fd43ae087a37527f5ad2bb9e16d5e847a7ebba
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-03-16 19:53:22 +0100

    try linux-builder

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -12,4 +12,27 @@
   nix.gc.automatic = true;
   # gets rid of duplicate store files
   nix.settings.auto-optimise-store = true;
+
+  # linux builder
+  # https://nixcademy.com/2024/02/12/macos-linux-builder/
+  # root was here previously, @admin is for linux-builder
+  nix.settings.trusted-users = [ "root" "@admin" ];
+  # sudo launchctl list org.nixos.linux-builder
+  # sudo launchctl stop org.nixos.linux-builder
+  nix.linux-builder = {
+    enable = true;
+    ephemeral = true;
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 40 * 1024; # 40gb
+          memorySize = 8 * 1024; # 8gb
+        };
+        cores = 6;
+      };
+    };
+  };
+  # try it
+  # nix build --impure  --expr '(with import <nixpkgs> { system = "aarch64-linux"; }; runCommand "foo" { nativeBuildInputs = [ neofetch ]; } "neofetch >> $out" )' && cat result
 }
