99788b2e00a3811ab9c6c0eb5eb138efb183ffb4
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-04-27 09:46:17 +0200

    add comment with attempted hack to move Firefox.app to Applications

--- home/link-home-manager-installed-apps.nix
+++ home/link-home-manager-installed-apps.nix
@@ -21,6 +21,15 @@
         -t l -d 1 . ${apps}/Applications \
       	-x $DRY_RUN_CMD ${pkgs.mkAlias} -L {} "$tmp_path/{/}"
       $DRY_RUN_CMD rm -rf "$app_path"
+
+      # try to copy Firefox.app to /Applications
+      # for file in $tmp_path/*; do
+      #   rm -rf "/Applications/$(basename "$file")"
+      #   tmp=$(/usr/bin/osascript -e "tell application \"Finder\" to get the POSIX path of (original item of alias POSIX file \"$file\" as alias)")
+      #   file="''${tmp%/}"
+      #   cp -fHRL "$file" "/Applications/$(basename "$file")"
+      # done
+
       $DRY_RUN_CMD mkdir -p "$app_path"
       $DRY_RUN_CMD mv "$tmp_path" "$app_path"
     '';
