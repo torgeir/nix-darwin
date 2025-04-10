64219b97dc967343ab175fd9186aaef3b5acaf24
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-04-10 19:52:27 +0200

    pkgs.unstable.colima

--- home/docker.nix
+++ home/docker.nix
@@ -4,8 +4,10 @@
 
     # nix 23.11 is colima 0.5.6
     # https://github.com/abiosoft/colima/issues/913
-    # try unstable which is colima 0.6.7
-    pkgs.unstable.colima # colima start --edit to tune its resources
+    # try unstable which is colima 0.8.1
+    pkgs.unstable.colima
+    # colima start --edit to tune its resources
+    # colima start --vz-rosetta for emulating x86 (sql server need this)
 
     docker
     docker-compose
