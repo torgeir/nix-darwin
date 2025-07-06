48f6bed8f9d8beac61e6c492c14b965ccc3a0d3d
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-07-06 09:03:44 +0200

    gpg 1password pinentry

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,33 +1,31 @@
-{ config, lib, pkgs, ... }: {
+{ config, inputs, lib, pkgs, ... }:
+
+let dotfiles = inputs.dotfiles;
+in {
 
   programs.gpg = {
     enable = true;
     package = pkgs.gnupg24;
   };
 
-  home.packages = with pkgs; [ pinentry_mac ];
-
   # gnu --batch --import
   #  https://github.com/NixOS/nixpkgs/issues/240819#issuecomment-1616760598
   home.file.".gnupg/gpg-agent.conf".text = ''
     # seconds after the last GnuPG activity
-    default-cache-ttl 28800
-    default-cache-ttl-ssh 28800
+    default-cache-ttl 600
+    default-cache-ttl-ssh 600
 
-    # seconds it caches after entering your password
-    max-cache-ttl 28800
-    max-cache-ttl-ssh 28800
+    # max seconds to cache, after extended periods
+    max-cache-ttl 1800
+    max-cache-ttl-ssh 1800
 
-    # allow presetting the passphrase from 1password
-    allow-preset-passphrase
-
-    # mac
-    pinentry-program "${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac";
+    # gpg 1password prompt
+    pinentry-program "${
+      lib.getExe (pkgs.writeShellScriptBin "gpg-1p-pinentry"
+        (builtins.readFile (dotfiles + "/gpg-1p-pinentry")))
+    }"
 
     enable-ssh-support
-
-    # remember to reload it after editing this file
-    #  killall gpg-agent && gpg-connect-agent reloadagent /bye
 
     # prevent scdaemon error messages in syslog regarding a smart card reader
     # you don't have one
